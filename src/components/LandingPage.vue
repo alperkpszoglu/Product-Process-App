44e47e027c3fdda762820c0cc15060067312a45f
Alper Kapusızoğlu <alperkapusizoglu@gmail.com>
2022-12-14 00:22:07 +0300

    completed template

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,12 +1,29 @@
 <template>
-  <navbar-component></navbar-component>
+  <div>
+    <navbar-component />
+    <button class="btn btn-primary btn-cs" @click="activeProd = 'product-list'">list</button>
+    <button class="btn btn-primary btn-cs" @click="activeProd = 'product-exit'">cikis</button>
+    <button class="btn btn-primary btn-cs" @click="activeProd = 'product-purchase'">purchase</button>
+    <component :is="activeProd"></component>
+  </div>
 </template>
 
 <script>
 import NavbarComponent from '../shared/Navbar';
+import ProductList from './ProductList';
+import ProductPurchase from './ProductPurchase';
+import ProductExit from './ProductExit';
 export default {
+  data() {
+    return {
+      activeProd: 'product-list',
+    };
+  },
   components: {
     NavbarComponent,
+    ProductList,
+    ProductPurchase,
+    ProductExit,
   },
 };
 </script>
--- src/shared/Navbar.vue
+++ src/shared/Navbar.vue
@@ -1,22 +1,19 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">ProdSimple</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#">Ürün Listesi <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="#">Ürün İşlem</a>
-        <a class="nav-item nav-link" href="#">Ürün Çıkış</a>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ürün Listesi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ürün İşlemleri</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ürün Çıkışı</a>
+            </li>
+        </ul>
       </div>
     </div>
   </nav>
