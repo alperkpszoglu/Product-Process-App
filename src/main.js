2576267a37c0fe1cc62127b88ea9a0aaa4cac4d7
Alper Kapusızoğlu <alperkapusizoglu@gmail.com>
2022-12-15 23:48:11 +0300

    vuex added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
-import {routes} from './routes';
+import { routes } from './routes';
+import { store } from './store/index';
 
 Vue.use(VueRouter);
 
@@ -12,5 +13,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
+  store,
   render: (h) => h(App),
 });
