d0fed28b8a3e3a6bc100892be1bfabdb2b018bcb
Alper Kapusızoğlu <alperkapusizoglu@gmail.com>
2022-12-24 22:03:19 +0300

    router configuration completed

--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import ProductList from './components/ProductList.vue';
 import ProductExit from './components/ProductExit';
 import ProductPurchase from './components/ProductPurchase';
 
-export const routes = [
+Vue.use(VueRouter);
+
+const routes = [
   { path: '/', component: ProductList, name: 'productList' },
   { path: '/product-purchase', component: ProductPurchase, name: 'productPurchase' },
   { path: '/product-exit', component: ProductExit, name: 'productExit' },
   { path: '*', redirect: '/' },
 ];
+export const router = new VueRouter({
+  routes,
+});
